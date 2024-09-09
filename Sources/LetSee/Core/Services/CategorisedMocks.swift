564323b8d678c7b819e9be6103b35ebfecfa28c1
Valentin J. <farshadjahanmanesh@gmail.com>
2024-09-09 15:59:49 +0200

    Update CategorisedMocks.swift

--- Sources/LetSee/Core/Services/CategorisedMocks.swift
+++ Sources/LetSee/Core/Services/CategorisedMocks.swift
@@ -12,6 +12,11 @@ public struct CategorisedMocks: Hashable {
 
     /// List of mocks belonging to the category
     public var mocks: [LetSeeMock]
+
+    public init(category: LetSeeMock.Category, mocks: [LetSeeMock]) {
+        self.category = category
+        self.mocks = mocks
+    }
 }
 
 public typealias RequestToMockMapper = ((_: URL, _ mocks: Dictionary<String, Set<LetSeeMock>>) -> CategorisedMocks?)
